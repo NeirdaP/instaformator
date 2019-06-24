a57a4236ff9fffc197f18eb57dc6576ff157be36
NeirdaP <adripatriz@gmail.com>
2019-06-25 01:29:46 +0200

    Upload V1

---
+++ README.md
@@ -0,0 +1,13 @@
+# Instaformator
+Python Scripts which generate white edges on images so they fit the instagram ratios (1:1, 4:5 or 4:3).
+
+## Usage
+- Put the script in a folder filled with images
+- Go to your console and write `python3 ./instaformator.py`
+- The generated pictures should be in a new folder named *Edged*
+
+## Installation
+You need to install *Pillow* to use this script :
+```python3
+pip3 install Pillow
+```
---
+++ instaformator.py
@@ -0,0 +1,50 @@
+from PIL import Image
+import os
+
+def new_image_from_original_size(width, height):
+	return Image.new('RGB', (width, height), (255,255,255))
+
+
+def generate_edged_image(filepath):
+	oImg = Image.open(filepath)
+
+	oWidth, oHeight = oImg.size
+	ratio = oWidth/oHeight
+
+	yOffset = 0
+	xOffset = 0
+
+	if ratio > 1.1:
+		# image horizontale
+		# format 4:3
+		width = oWidth
+		height = int(width * 3 / 4)
+		yOffset = int(height / 2 - oHeight / 2)
+
+	elif ratio < 0.9:
+		# image verticale
+		# format 4:5
+		height = oHeight
+		width = int(height * 4 / 5)
+		xOffset = int(width / 2 - oWidth / 2)
+
+	else:
+		# image 1x1
+		return
+
+	canvas = new_image_from_original_size(width, height)
+	canvas.paste(oImg, (xOffset, yOffset))
+
+
+	if not os.path.exists(export_directory):
+		os.mkdir(export_directory)
+	export_name = export_directory + "/" +  filepath[0:-4] + "_edged.jpg"
+	canvas.save(export_name , "JPEG")
+
+
+export_directory = "Edged"
+
+imageFiles = [file for file in os.listdir() if file.endswith(".jpg")]
+
+for file in imageFiles:
+	generate_edged_image(file)
