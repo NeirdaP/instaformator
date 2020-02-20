f176c92cfac8022d592631de6ffd391cced3b493
NeirdaP <adripatriz@gmail.com>
2020-02-20 03:20:22 +0100

    Check if image exists in Published folder before exporting

--- instaformator.py
+++ instaformator.py
@@ -1,51 +1,55 @@
 from PIL import Image
 import os
 
+
 def new_image_from_original_size(width, height):
-	return Image.new('RGB', (width, height), (255,255,255))
+    return Image.new('RGB', (width, height), (255, 255, 255))
 
 
-def generate_edged_image(filepath):
-	oImg = Image.open(filepath)
+def generate_edged_image(filename):
+    oImg = Image.open(filename)
+    oWidth, oHeight = oImg.size
+    ratio = oWidth/oHeight
 
-	oWidth, oHeight = oImg.size
-	ratio = oWidth/oHeight
+    yOffset = 0
+    xOffset = 0
 
-	yOffset = 0
-	xOffset = 0
+    if ratio > 1.1:
+        # image horizontale
+        # format 4:3
+        width = oWidth
+        height = int(width * 3 / 4)
+        yOffset = int(height / 2 - oHeight / 2)
 
-	if ratio > 1.1:
-		# image horizontale
-		# format 4:3
-		width = oWidth
-		height = int(width * 3 / 4)
-		yOffset = int(height / 2 - oHeight / 2)
+    elif ratio < 0.9:
+        # image verticale
+        # format 4:5
+        height = oHeight
+        width = int(height * 4 / 5)
+        xOffset = int(width / 2 - oWidth / 2)
 
-	elif ratio < 0.9:
-		# image verticale
-		# format 4:5
-		height = oHeight
-		width = int(height * 4 / 5)
-		xOffset = int(width / 2 - oWidth / 2)
+    else:
+        # image 1x1
+        width = oWidth
+        height = oHeight
 
-	else:
-		# image 1x1
-		width = oWidth
-		height = oHeight
+    canvas = new_image_from_original_size(width, height)
+    canvas.paste(oImg, (xOffset, yOffset))
 
-	canvas = new_image_from_original_size(width, height)
-	canvas.paste(oImg, (xOffset, yOffset))
+    if not os.path.exists(export_directory):
+        os.mkdir(export_directory)
 
-	if not os.path.exists(export_directory):
-		os.mkdir(export_directory)
+    export_path = export_directory + "/" + filename[0:-4] + "_edged.jpg"
+    publish_path = "./2.Published/" + filename[0:-4] + "_edged.jpg"
 
-	export_name = export_directory + "/" +  filepath[0:-4] + "_edged.jpg"
-
-	if not os.path.exists(export_name):
-		canvas.save(export_name , "JPEG")
-		print("> Creates ", export_name)
-	else:
-		print(export_name, " already exists")
+    if not os.path.exists(publish_path):
+        if not os.path.exists(export_path):
+            canvas.save(export_path, "JPEG")
+            print("> Creates ", export_path)
+        else:
+            print(export_path, " already exists")
+    else:
+        print(publish_path, " already exists")
 
 
 export_directory = "Edged"
@@ -53,4 +57,4 @@ export_directory = "Edged"
 imageFiles = [file for file in os.listdir() if file.endswith(".jpg")]
 
 for file in imageFiles:
-	generate_edged_image(file)
+    generate_edged_image(file)
